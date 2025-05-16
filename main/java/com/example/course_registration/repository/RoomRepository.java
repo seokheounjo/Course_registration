7ec9fe97e55abd9f685af0609d120d647daa74f9
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-05-16 18:56:15 +0900

    Delete main directory

--- main/java/com/example/course_registration/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.course_registration.repository;
-
-import com.example.course_registration.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
