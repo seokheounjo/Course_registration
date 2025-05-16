901516739933c0f2e02c03e3667ebb238acac9f8
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-05-16 18:56:22 +0900

    Delete src directory

--- src/main/java/com/example/Course_registration/repository/ProfessorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Course_registration.repository;
-
-import com.example.Course_registration.domain.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> { }
