d61a9ebc29dc4f034b9563bc8fed93e0133a8388
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-05-18 02:59:56 +0900

    Delete Course_registration/src/main/java/com/example/Course_registration directory

--- Course_registration/src/main/java/com/example/Course_registration/repository/SubjectScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Course_registration.repository;
-
-import com.example.Course_registration.entity.SubjectSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectScheduleRepository extends JpaRepository<SubjectSchedule, Long> {
-}
