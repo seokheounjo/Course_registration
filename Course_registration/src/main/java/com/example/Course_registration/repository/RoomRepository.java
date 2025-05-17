699345688c0116d34c6f68a73b3d3d2480f8126a
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-05-18 03:03:00 +0900

    Delete Course_registration/src directory

--- Course_registration/src/main/java/com/example/Course_registration/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Course_registration.repository;
-
-import com.example.Course_registration.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
