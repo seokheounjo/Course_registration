3d54c82faac2345da35c42ab74bbb6defa645a15
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-05-21 08:59:16 +0900

    Delete Course_registration directory

--- Course_registration/src/main/java/com/example/Course_registration/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-// 6. AdminRepository.java
-package com.example.Course_registration.repository;
-
-import com.example.Course_registration.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
-}
