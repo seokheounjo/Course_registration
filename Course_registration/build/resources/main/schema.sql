1ceca6b9e5cda35a07279625b9f4fbfed5700dcc
프로용병 <151607105+seokheounjo@users.noreply.github.com>
2025-06-07 21:19:10 +0900

    Delete Course_registration directory

--- Course_registration/build/resources/main/schema.sql
+++
@@ -1,3 +0,0 @@
--- schema.sql 파일 내용 수정
--- 이미 컬럼이 존재하는 경우 오류가 발생하지 않도록 IF NOT EXISTS 추가
-ALTER TABLE subjects ADD COLUMN IF NOT EXISTS target_grade VARCHAR(10) DEFAULT '전체';
--- Course_registration/src/main/java/com/example/registrationweb/repository/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.registrationweb.repository;
-
-import com.example.registrationweb.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
