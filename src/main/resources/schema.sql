765c4773d24eef77633b3330474064ddf023c5b0
seokheounjo <untjrgus@naver.com>
2025-09-11 14:45:31 +0900

    Fix schema.sql - add department column to students table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS students (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     grade VARCHAR(10) NOT NULL,
+    department VARCHAR(100),
     birth_date DATE,
     phone VARCHAR(20),
     address TEXT,
